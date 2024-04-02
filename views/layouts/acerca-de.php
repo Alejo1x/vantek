0ba3412605cb982e09943b69996507a0d4b017c0
Alejo1x <165846884+Alejo1x@users.noreply.github.com>
2024-04-02 15:52:08 -0600

    Add files via upload

---
+++ views/layouts/acerca-de.php
@@ -0,0 +1,122 @@
+   <!-- Sección de Acerca de -->
+   <section class="seccion-2" id="acerca-de">
+
+    <div class="info__inicio-acerca">
+      <H1 style="font-size:25px;">TECH-WHEELS</H1>
+      <hr>
+      <h1>Este proyecto consiste en una aplicación movil nativa. La cual permite registrar y gestionar los datos de una empresa que renta de autos. Se emplearon las siguientes herramientas de desarrollo:</h1>
+    </div>
+
+   <div id="carouselExampleCaptions" class="carousel slide">
+      <div class="carousel-indicators">
+         <button id="btn__slider" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+         <button id="btn__slider" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+         <button id="btn__slider" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+         <button id="btn__slider" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+         <button id="btn__slider" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
+         
+      </div>
+      <div class="carousel-inner">
+         <div class="carousel-item active">
+            <img id="img__carousel" src="../images/img-androistudio.png" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block p-0">
+            <h5 class="title-carousel"></h5>
+            <p class="text-carousel">Entorno de desarrollo integrado enfocado a aplicaciones móviles</p>
+            </div>
+         </div>
+         <div class="carousel-item">
+            <img id="img__carousel" src="../images/img-balsamiq.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block p-0">
+            <h5 class="title-carousel"></h5>
+            <p class="text-carousel">Herramienta rápida wireframing que le ayuda a trabajar más rápido y más inteligente.</p>
+            </div>
+         </div>
+         <div class="carousel-item">
+            <img id="img__carousel" src="../images/img-adobexd.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block p-0">
+            <h5 class="title-carousel">Adobe XD</h5>
+            <p class="text-carousel">Herramienta de edición gráfica que sirve para la creación de interfaces.</p>
+            </div>
+         </div>
+         <div class="carousel-item">
+            <img id="img__carousel" src="../images/img-uml.jpg" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block p-0">
+            <h5 class="title-carousel">Star UML</h5>
+            <p class="text-carousel">Herramienta de modelado de software de código abierto que se utiliza para la creación y visualización de diagramas.</p>
+            </div>
+         </div>
+         <div class="carousel-item">
+            <img id="img__carousel" src="../images/img-sqlite.webp" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block p-0">
+            <h5 class="title-carousel">SQLite</h5>
+            <p class="text-carousel">Sistema de gestión de bases de datos ligeras para aplicaciones móviles</p>
+            </div>
+         </div>
+
+
+      </div>
+
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="visually-hidden">Next</span>
+      </button>
+   </div>
+
+   <div class="accordion accordion-flush" id="accordionFlushExample">
+  <div class="accordion-item ">
+    <h2 class="accordion-header" id="flush-headingOne">
+      <button style="font-size:15px; font-weight:bold;" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+        Android Studio
+      </button>
+    </h2>
+    <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body" style="font-size:12px;">Android Studio es un entorno de desarrollo integrado (IDE) utilizado principalmente para crear aplicaciones móviles para dispositivos Android.</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingTwo">
+      <button style="font-size:15px; font-weight:bold;" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+        Balsamiq Wireframes
+      </button>
+    </h2>
+    <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body" style="font-size:12px;">Balsamiq Wireframes es una herramienta de diseño de interfaces de usuario que permite crear rápidamente bocetos y prototipos de baja fidelidad para sitios web y aplicaciones móviles.</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingThree">
+      <button style="font-size:15px; font-weight:bold;" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+        Adobe XD
+      </button>
+    </h2>
+    <div id="flush-collapseThree" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body" style="font-size:12px;">Adobe XD es una aplicación de diseño y prototipado de experiencias de usuario (UX) y diseño de interfaces de usuario (UI).</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingFour">
+      <button style="font-size:15px; font-weight:bold;" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseThree">
+        Star UML
+      </button>
+    </h2>
+    <div id="flush-collapseFour" class="accordion-collapse collapse" aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body" style="font-size:12px;">StarUML es una herramienta de modelado UML (Lenguaje de Modelado Unificado) que permite a los desarrolladores diseñar y visualizar estructuras de software y sistemas.</div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="flush-headingFive">
+      <button style="font-size:15px; font-weight:bold;" class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseThree">
+        SQLite
+      </button>
+    </h2>
+    <div id="flush-collapseFive" class="accordion-collapse collapse" aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+      <div class="accordion-body" style="font-size:12px;">SQLite es un sistema de gestión de bases de datos relacional que se utiliza principalmente para bases de datos embebidas y aplicaciones ligeras.</div>
+    </div>
+  </div>
+</div>
+  </section>
+ <!-- Fin de sección de Acerca de -->
---
+++ views/layouts/objetivo.php
@@ -0,0 +1,61 @@
+<!-- Seccion Objetivo -->
+<section id="obj">
+        <div class="info-objetivo">
+                <div id="objgen" data-aos="zoom-in">
+                    <h2 class="title">Objetivo General</h2>
+                    <hr style="padding-top:20px;">
+                    <p style="padding-top:20px;" class="text">Desarrollar una aplicación móvil nativa que ofrezca un servicio integral de renta de transporte terrestre, abarcando tanto la logística de carga como el transporte de personal. La aplicación proporciona a los usuarios una plataforma fácil de usar y segura, permitiéndoles explorar, reservar y gestionar eficientemente diferentes tipos de vehículos para satisfacer sus necesidades de transporte.</p>
+                </div>
+        </div>
+       
+        <div class="contenedor">
+            <!-- tarjeta 1 -->
+		    <div class="contenedor_tarjeta" data-aos="zoom-in">
+                <a href="">
+                    <figure id="tarjeta">
+                        <img src="../images/card-crear.jpg" class="frontal" alt="">
+                        <figcaption class="trasera">
+                            <hr>
+                            <p class="text">Crear una interfaz de administrador amigable que 
+                                le permita navegar de manera simple por la aplicación para facilitar 
+                                la exploración de opciones de transporte, ya sea para carga o transporte personal.</p>
+                        </figcaption>
+                    </figure>
+                </a>
+            </div>
+
+            <!-- tarjeta 2 -->
+            <div class="contenedor_tarjeta" data-aos="zoom-in">
+                <a href="">
+                    <figure id="tarjeta">
+                        <img src="../images/card-implementar.jpeg" class="frontal" alt="">
+                        <figcaption class="trasera">
+                            <hr>
+                            <p class="text">Implementar una aplicación de renta que permita al administrador 
+                                seleccionar, programar y confirmar de manera rápida y sencilla con un sistema de
+                                notificaciones para informar al administrador sobre el estado de las rentas.</p>
+                        </figcaption>
+                    </figure>
+                </a>
+            </div>
+
+            <!-- tarjeta 3 -->
+            <div class="contenedor_tarjeta" data-aos="zoom-in">
+                <a href="">
+                    <figure id="tarjeta">
+                        <img src="../images/card-establecer.png" class="frontal" alt="">
+                        <figcaption class="trasera">
+                            <hr>
+                            <p class="text">Establecer un sistema de seguridad robusto que garantice la 
+                                privacidad de los datos del usuario y brinde protección contra posibles 
+                                amenazas, como ataques cibernéticos, asegurando la integridad de la información 
+                                sensible almacenada en la aplicación.</p>
+                        </figcaption>
+                    </figure>
+                </a>
+            </div>
+
+	    </div>
+       
+    </section>
+<!-- Fin Seccion Objetivo -->
